d097d0b8128ae057e0ac7035175cfc8379008c10
Nathan Santos de Deus <nathandosantos@hotmail.com>
2025-09-30 14:22:41 +0100

    Magic 8 ball

--- oop-2025-a-0930-77304/Program.cs
+++ oop-2025-a-0930-77304/Program.cs
@@ -1,2 +1,33 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+
+List<string> magicEightBallAnswers = new List<string>
+{
+    "It is certain.",
+    "It is decidedly so.",
+    "Without a doubt.",
+    "Yes – definitely.",
+    "You may rely on it.",
+    "As I see it, yes.",
+    "Most likely.",
+    "Outlook good.",
+    "Yes.",
+    "Signs point to yes.",
+    "Reply hazy, try again.",
+    "Ask again later.",
+    "Better not tell you now.",
+    "Cannot predict now.",
+    "Concentrate and ask again.",
+    "Don't count on it.",
+    "My reply is no.",
+    "My sources say no.",
+    "Outlook not so good.",
+    "Very doubtful."
+};
+
+Random random = new Random();
+string randomMagicEightBallAnswer = magicEightBallAnswers[random.Next(magicEightBallAnswers.Count)];
+
+// Display the result
+Console.WriteLine("Randomly selected answer: " + randomMagicEightBallAnswer);
